ced8ac77a0129404dde42178b2b1d930c3c7356d
TheREX <shivamtherexpandey@gmail.com>
2025-11-29 11:18:58 +0530

    Minor Bug Fixes and Improvement.

--- usm_home/usm_home/urls.py
+++ usm_home/usm_home/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from usm_home.view import Redirect
 
 urlpatterns = [
+    path('', Redirect.as_view()),
     path('admin/', admin.site.urls),
     path(settings.APPLICATION_PREFIX, include('usm_interface.urls')),
     path(settings.APPLICATION_PREFIX + 'api/user/', include('usm_user.urls')),
---
+++ usm_home/usm_home/view.py
@@ -0,0 +1,6 @@
+from django.views import View
+from django.shortcuts import redirect
+
+class Redirect(View):
+    def get(self, request):
+        return redirect('landing_page')
